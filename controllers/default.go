fa8330f0cd3f795c5d77f5637ba1e0b579e325c4
ying-tang <tyying@cn.ibm.com>
2019-10-17 14:55:01 +0800

    Add no-store header to index.html
    dd no-store header to index.html to stop content from being cached

--- controllers/default.go
+++ controllers/default.go
@@ -10,5 +10,12 @@ type MainController struct {
 
 func (this *MainController) Get() {
 	this.TplName = "index.html"
+	this.Ctx.Output.Header("Cache-Control", "no-cache, no-store, must-revalidate")
 	this.Render()
 }
+
+func (this *MainController) Get2() {
+	this.TplName = "detection-test-object.html"
+	this.Render()
+}
+
